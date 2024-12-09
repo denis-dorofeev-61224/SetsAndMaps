56367dc053bd24131f4d94d84f9b4a6463d0e458
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2024-12-09 20:52:23 +0300

    Branch of developer_2 done.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello  brave new world!");
+        System.out.println("Hi! This is branch of developer_2");
 
     }
 }
